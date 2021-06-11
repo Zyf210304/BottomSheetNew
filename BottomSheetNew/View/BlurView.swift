6522328cf0b29277c14d89dfe9820ddfbd866abd
张亚飞 <672233580@qq.com>
2021-06-11 17:15:24 +0800

    飞

---
+++ BottomSheetNew/View/BlurView.swift
@@ -0,0 +1,27 @@
+//
+//  BlurView.swift
+//  BottomSheetNew
+//
+//  Created by 张亚飞 on 2021/6/11.
+//
+
+import SwiftUI
+
+struct BlurView: UIViewRepresentable {
+    
+    var style: UIBlurEffect.Style
+    
+    func makeUIView(context: Context) -> UIVisualEffectView {
+        
+        let view = UIVisualEffectView(effect: UIBlurEffect(style: style))
+        
+        return view
+    }
+
+    func updateUIView(_ uiView: UIVisualEffectView, context: Context) {
+        
+    }
+    
+    
+}
+
---
+++ BottomSheetNew/View/CustomCorner.swift
@@ -0,0 +1,23 @@
+//
+//  CustomCorner.swift
+//  BottomSheetNew
+//
+//  Created by 张亚飞 on 2021/6/11.
+//
+
+import SwiftUI
+
+struct CustomCorner: Shape {
+    
+    var corners: UIRectCorner
+    var radius: CGFloat
+    
+    func  path(in rect: CGRect) -> Path {
+        
+        let path = UIBezierPath(roundedRect: rect, byRoundingCorners: corners, cornerRadii: CGSize(width: radius, height: radius))
+        
+        return Path(path.cgPath)
+    }
+}
+
+
