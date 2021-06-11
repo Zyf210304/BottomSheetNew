0e2e558b1bd62f3f525777e1e8490dcb16dee7d6
张亚飞 <672233580@qq.com>
2021-06-11 13:36:59 +0800

    Initial Commit

---
+++ BottomSheetNew/BottomSheetNewApp.swift
@@ -0,0 +1,17 @@
+//
+//  BottomSheetNewApp.swift
+//  BottomSheetNew
+//
+//  Created by 张亚飞 on 2021/6/11.
+//
+
+import SwiftUI
+
+@main
+struct BottomSheetNewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
